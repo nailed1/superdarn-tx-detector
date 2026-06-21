f0e1d78312e0d37342d30bf59ebc36d794b95c52
nailed1 <max20070608@gmail.com>
2026-06-21 21:15:22 +0800

    feat: add header files with type definitions and RST includes

---
+++ src/defs.h
@@ -0,0 +1,11 @@
+#include "rtypes.h"
+
+#ifndef _DEFS_H_
+#define _DEFS_H_
+typedef short int int16;
+typedef int int32;
+typedef unsigned short int uint16;
+typedef unsigned int uint32;
+#include <zlib.h>
+
+#endif
---
+++ src/main.h
@@ -0,0 +1,18 @@
+#include "defs.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "radar.h"
+
+#include "rtypes.h"
+#include "dmap.h"
+//#include "limit.h"
+#include "rprm.h"
+#include "fitdata.h"
+#include "fitread.h"
+#include "fitwrite.h"
+
+#include "invmag.h"
